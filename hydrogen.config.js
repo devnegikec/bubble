611091523811ce2ceb3911ecf1ab78282a806554
Devendra Singh <devnegikec@gmail.com>
2022-08-09 11:00:41 +0530

    hydrogen hellowworld template app

---
+++ hydrogen.config.js
@@ -0,0 +1,9 @@
+import {defineConfig} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    storeDomain: 'clube-charakhee.myshopify.com',
+    storefrontToken: '',
+    storefrontApiVersion: '2022-07',
+  },
+});
