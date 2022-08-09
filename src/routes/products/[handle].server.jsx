8ee2e59d86b4a6006099e769a39d497469455c86
Devendra Singh <devnegikec@gmail.com>
2022-08-09 19:14:49 +0530

    added some configrutaion

--- src/routes/products/[handle].server.jsx
+++ src/routes/products/[handle].server.jsx
@@ -1,15 +1,17 @@
+import { Suspense } from "react";
 import {
     gql,
+    Seo,
     useShopQuery,
-    useServerAnalytics,
     useRouteParams,
-    ShopifyAnalyticsConstants,
-    Seo
+    useServerAnalytics,
+    ProductOptionsProvider,
+    ShopifyAnalyticsConstants
   } from "@shopify/hydrogen";
 import { ShopifyServerAnalyticsConnector } from "@shopify/hydrogen/config";
-  import { Suspense } from "react";
 
-  import { Layout } from "../../components/Layout.server";
+
+  import { Layout } from "~/components/Layout.server";
 //   import ProductDetails from "../../components/ProductDetails.client";
   
   export default function Product({ params }) {
@@ -22,7 +24,8 @@ import { ShopifyServerAnalyticsConnector } from "@shopify/hydrogen/config";
         variables: {
             handle
         }
-    })
+    });
+
 
     useServerAnalytics({
         shopify: {
@@ -36,16 +39,7 @@ import { ShopifyServerAnalyticsConnector } from "@shopify/hydrogen/config";
         <Suspense>
             <Seo type="product" data={product} />
         </Suspense>
-        <section className="w-full overflow-x-hidden gap-4 md:gap-8 grid px-6 md:px-8 lg:px-12">
-            <div className="grid gap-2 mt-10">
-            <h1 className="text-4xl font-bold leading-10 whitespace-normal">
-                {product.title}
-            </h1>
-            <span className="max-w-prose whitespace-pre-wrap inherit text-copy opacity-50 font-medium">
-                {product.vendor}
-            </span>
-            </div>
-        </section>
+        {/* <ProductDetails product={product} /> */}
       </Layout>
     );
   }
--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import hydrogen from '@shopify/hydrogen/plugin';
 
 export default defineConfig({
   plugins: [hydrogen()],
+  resolve: {
+    alias: [{find: /^~\/(.*)/, replacement: '/src/$1'}]
+  },
+  optimizeDeps: {
+    include: ['@headlessui/react', 'clsx', 'react-use', 'typographic-base'],
+  }
 });
