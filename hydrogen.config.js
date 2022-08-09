89f4396ef1ac6dc4102f77a361d910a4edb93638
Devendra Singh <devnegikec@gmail.com>
2022-08-09 13:21:30 +0530

    collection page show products

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -3,7 +3,7 @@ import {defineConfig} from '@shopify/hydrogen/config';
 export default defineConfig({
   shopify: {
     storeDomain: 'clube-charakhee.myshopify.com',
-    storefrontToken: '',
+    storefrontToken: 'e0ee9456b9ec62859b5d6004fbb57d28',
     storefrontApiVersion: '2022-07',
   },
 });
